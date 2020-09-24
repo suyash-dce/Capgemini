db496f22b836394c0f61e199ab5bb62df0a5dabe
Suyash Jain <jain.suyash28@gmail.com>
2020-09-24 10:54:15 +0530

    First Commit

---
+++ employee.java
@@ -0,0 +1,7 @@
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		}
+	}

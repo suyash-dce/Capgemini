4a4221e6886e29a5402b19f13554d1eca75933f9
Suyash Jain <jain.suyash28@gmail.com>
2020-09-24 11:10:17 +0530

    Second Branch

---
+++ emphours.java
@@ -0,0 +1,27 @@
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int empHours=0;
+		int empWage=0;
+		
+		int empCategory = (int) Math.floor((Math.random()*10)%3);
+		
+		switch(empCategory) {
+			case 0:
+				System.out.println("Employee is Full Time");
+				empHours = 8;
+				break;
+			case 1:
+				System.out.println("Employee is Part Time");
+				empHours = 8;
+				break;
+			default:
+				empHours =0;					
+					
+		}
+		empWage = empHours*20;
+ 		System.out.println("Wage of Employee: "+empWage);
+ 		}
+	}
